aad9e2226e2a4af7828cf46438919f33500a6b51
Giulianno Ramos <giuleramos@gmail.com>
2022-09-09 20:25:30 -0300

    https://github.com/giuliannoramos/EntradasUsuario-JsHtml.git

--- app/Index.js
+++ app/Index.js
@@ -1,3 +1,33 @@
 function NovaEntrada() {    
 
+    var form = document.querySelector('#form');
+
+    var entrada = document.createElement('input');
+    entrada.type = 'n1';
+    entrada.classList.add('entrada');
+
+    var botao = document.createElement('input');
+    botao.type = 'button';
+    botao.value = '+';
+    
+    botao.onclick = NovaEntrada;
+    form.appendChild(entrada);
+    form.appendChild(botao);
+    Somar();
+}
+
+function Somar()
+{
+    var form = document.querySelectorAll('.entrada');    
+
+    let somatorio = 0;
+    
+    form.forEach(function(e){
+       if(e.value !== '') {
+        somatorio+= parseInt(e.value); 
+       }        
+    });
+    alert(somatorio);
+    let divRes = document.querySelector('#dadosSalvos');
+    divRes.innerHTML = '<h1>'+somatorio+'</h1>'
 }
